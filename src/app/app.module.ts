c1a0774449f3652ef2f397545e9e139122552e3c
Hélio Zumba <helio.zumba@digitalfactory.co.ao>
2021-03-15 18:05:26 +0100

    first configs + login form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { LoginFormComponent } from './views/login/login-form/login-form.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      {path: '**', component: NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
