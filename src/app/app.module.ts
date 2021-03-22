1df10fc74a72f5b060c99f55885a7ecb041bb666
Hélio Zumba <helio.zumba@digitalfactory.co.ao>
2021-03-22 11:10:42 +0100

    post-add structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { RouterModule } from '@angular/router'
 import { LoginComponent } from './views/login/login.component'
 import { LoginFormComponent } from './views/login/login-form/login-form.component'
 import { NotFoundComponent } from './views/not-found/not-found.component'
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { SidebarComponent } from './views/dashboard/sidebar/sidebar.component';
-import { HeaderComponent } from './views/dashboard/header/header.component';
-import { ViewerComponent } from './views/dashboard/viewer/viewer.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component'
+import { SidebarComponent } from './views/dashboard/sidebar/sidebar.component'
+import { HeaderComponent } from './views/dashboard/header/header.component'
+import { ViewerComponent } from './views/dashboard/viewer/viewer.component'
 import { PostListComponent } from './views/dashboard/viewer/post-list/post-list.component'
 
 @NgModule({
@@ -29,7 +29,14 @@ import { PostListComponent } from './views/dashboard/viewer/post-list/post-list.
       {
         path: 'dashboard',
         component: DashboardComponent,
-        data: { title: 'Jucas' }
+        data: { title: 'Jucas' },
+        children: [
+          {
+            path: 'post-list',
+            component: PostListComponent,
+            data: { title: 'Posts' }
+          }
+        ]
       }
     ])
   ],
